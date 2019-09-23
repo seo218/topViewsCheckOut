aab1a8a88a6b23d5edf589c728ab4d0b474fceee
seo218 <casey.seo1@gmail.com>
2019-09-23 15:37:58 -0400

    add to cart promisfying in progress

--- src/components/reservationPage.js
+++ src/components/reservationPage.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { products } from '../../database/bikerentals.js'
-import { Carousel, Form, Col, Button, Dropdown, PopoverContent } from 'react-bootstrap'
+import { Carousel, Form, Col, Button, Dropdown, Row, FormLabel, Figure } from 'react-bootstrap'
+import { resolve } from 'path'
+import { rejects } from 'assert'
 
 class ReservationPage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            cartItems: [],
+            cartItems: [{ name: "" }],
             currentItem: { name: "Select Product" },
-            cartNumberOfItems: 0
-
+            cartNumberOfItems: 0,
         }
         this.getCarouselItems = this.getCarouselItems.bind(this)
         this.getProductSelectionItems = this.getProductSelectionItems.bind(this)
         this.updateProductButton = this.updateProductButton.bind(this)
         this.addToCart = this.addToCart.bind(this)
+        this.getCartDisplayItems = this.getCartDisplayItems.bind(this)
     }
 
     getCarouselItems() {
@@ -65,18 +67,57 @@ class ReservationPage extends React.Component {
     }
 
     addToCart() {
-        let newCart = this.state.cartItems
-        newCart.push(this.state.currentItem)
-        this.setState({
-            cartItems: newCart,
-            cartNumberOfItems: this.state.cartItems.length
-        })
+        // let newCart = this.state.cartItems
+        // newCart.push(this.state.currentItem)
+        // let stateUpdate = new Promise((resolve, reject) => {
+        //     resolve(
+        //         this.setState({
+        //             cartItems: newCart,
+        //             cartNumberOfItems: this.state.cartItems.length,
+        //             cartDisplay: this.getCartDisplayItems()
+        //         })
+        //     )
+        // })
+        // stateUpdate()
+        // .then(()=>{
+        //     console.log('working')
+        // })
+        // .catch(()=>{
+        //     console.log('notworking')
+        // })
+
     }
+
+    getCartDisplayItems() {
+        let items = []
+        // console.log('printing state cart items', this.state.cartItems[1].name)
+        for (let i = 0; i < this.state.cartItems; i++) {
+            items.push(
+                <Figure>
+                    <Figure.Image
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src="holder.js/171x180"
+                    />
+                <Figure.Caption>
+                Nulla vitae elit libero, a pharetra augue mollis interdum.
+                 </Figure.Caption>
+                </Figure>
+            )
+          
+                
+        }
+        // console.log("printing itmes in getDisplay item", items)
+        return items
+    }
+
 
     render() {
         return (
             <div className="productSelectionPage">
                 {/* testing line */}
+
                 <div> {console.log(
                     "printing cart items", this.state.cartItems,
                     "printing current item", this.state.currentItem,
@@ -90,27 +131,36 @@ class ReservationPage extends React.Component {
                 </div>
 
                 <div className="cart">
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Cart ( {this.state.cartNumberOfItems} Items )</Form.Label>
-                        <br></br>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                {this.state.currentItem.name}
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                {this.getProductSelectionItems()}
+                    <Form>
+                        <Row>
+                            <Col className="addToCart">
+                                <Form.Label>Cart ( {this.state.cartNumberOfItems} Items )</Form.Label>
+                                <br></br>
+                                <Dropdown>
+                                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                        {this.state.currentItem.name}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        {this.getProductSelectionItems()}
 
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        <br></br>
-                        <Button
-                            onClick={() => {
-                                this.addToCart();
-                            }}
-                        >
-                            Add to Cart
-                            </Button>
-                    </Form.Group>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <br></br>
+                                <Button
+                                    onClick={() => {
+                                        this.addToCart();
+                                    }}
+                                >
+                                    Add to Cart
+                        </Button>
+                            </Col>
+                            <Col className="cartDisplay">
+                                <FormLabel>Current Items</FormLabel>
+                                {this.getCartDisplayItems()}
+                                {[]}
+                            </Col>
+                        </Row>
+                    </Form>
                 </div>
 
             </div>
