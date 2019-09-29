274176ace5bea1ece83c49c237cf8de09f9969a7
seo218 <casey.seo1@gmail.com>
2019-09-29 15:58:29 -0400

    image update

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30614,37 +30614,37 @@ var products = [{
   "id": 1,
   "name": "Adult Male Bike",
   "price": 20.50,
-  "image": "http://via.placeholder.com/250x250?text=Adult%20Male%20Bike",
+  "image": "https://doc-0s-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/vtfijhk6oqe5br2ku107i16ftp8f624r/1569780000000/02903672740124267563/02903672740124267563/1KmwZUDDZ7GMiQRmdyzo5vG-pqb_2HkCb?e=view",
   "product_type": "bike"
 }, {
   "id": 2,
   "name": "Adult Female Bike",
   "price": 20.50,
-  "image": "http://via.placeholder.com/250x250?text=Adult%20Female%20Bike",
+  "image": "https://doc-0k-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/o1j8e3cvg33vpt9317alpcljp2qdkmtn/1569780000000/02903672740124267563/02903672740124267563/1oQBK_DsfTa6TXJL9OtMnDxs3R1VfpT7h?e=view",
   "product_type": "bike"
 }, {
   "id": 3,
   "name": "Kids Unisex Bike",
   "price": 12.75,
-  "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike",
+  "image": "https://doc-10-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/882ftgsfe76ae2659btbhlpj877ckasq/1569780000000/02903672740124267563/02903672740124267563/1aJNHeIzaWyt6zTdZYhVev4oDv_qg7C-c?e=view",
   "product_type": "bike"
 }, {
   "id": 4,
   "name": "Adult Unisex Helmet",
   "price": 4.00,
-  "image": "http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet",
+  "image": "https://doc-08-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/teq2cb7ikrvjobju1it57qesnpounhag/1569780000000/02903672740124267563/02903672740124267563/1ZDzyHbMXoBegiF2_YZqZRg6HrqQwMWGh?e=view",
   "product_type": "accessory"
 }, {
   "id": 5,
   "name": "Kids Unisex Helmet",
   "price": 3.50,
-  "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet",
+  "image": "https://doc-00-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/ae1a03s5litld35dla73grtqcsgea6bm/1569780000000/02903672740124267563/02903672740124267563/16DAt2ZOXN3Rjq2HlaoX-d-GJ5xT6Avq4?e=view",
   "product_type": "accessory"
 }, {
   "id": 6,
   "name": "Insurance",
   "price": 9.99,
-  "image": "http://via.placeholder.com/250x250?text=Insurance",
+  "image": "https://doc-08-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/j6tcq7huipvkp0affgkr80t664pdg7cj/1569780000000/02903672740124267563/02903672740124267563/1Jnw1d59DgofpaW8JIAzhyaZ96tv_rXqB?e=view",
   "product_type": "addon"
 }];
 module.exports = {
