cbc2d999c1a483b8c17552f699b4879952dbd30f
seo218 <casey.seo1@gmail.com>
2019-09-30 07:18:04 -0400

    photo update

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30614,37 +30614,37 @@ var products = [{
   "id": 1,
   "name": "Adult Male Bike",
   "price": 20.50,
-  "image": "https://doc-0s-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/vtfijhk6oqe5br2ku107i16ftp8f624r/1569780000000/02903672740124267563/02903672740124267563/1KmwZUDDZ7GMiQRmdyzo5vG-pqb_2HkCb?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=1KmwZUDDZ7GMiQRmdyzo5vG-pqb_2HkCb",
   "product_type": "bike"
 }, {
   "id": 2,
   "name": "Adult Female Bike",
   "price": 20.50,
-  "image": "https://doc-0k-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/o1j8e3cvg33vpt9317alpcljp2qdkmtn/1569780000000/02903672740124267563/02903672740124267563/1oQBK_DsfTa6TXJL9OtMnDxs3R1VfpT7h?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=1oQBK_DsfTa6TXJL9OtMnDxs3R1VfpT7h",
   "product_type": "bike"
 }, {
   "id": 3,
   "name": "Kids Unisex Bike",
   "price": 12.75,
-  "image": "https://doc-10-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/882ftgsfe76ae2659btbhlpj877ckasq/1569780000000/02903672740124267563/02903672740124267563/1aJNHeIzaWyt6zTdZYhVev4oDv_qg7C-c?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=1aJNHeIzaWyt6zTdZYhVev4oDv_qg7C-c",
   "product_type": "bike"
 }, {
   "id": 4,
   "name": "Adult Unisex Helmet",
   "price": 4.00,
-  "image": "https://doc-08-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/teq2cb7ikrvjobju1it57qesnpounhag/1569780000000/02903672740124267563/02903672740124267563/1ZDzyHbMXoBegiF2_YZqZRg6HrqQwMWGh?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=1ZDzyHbMXoBegiF2_YZqZRg6HrqQwMWGh",
   "product_type": "accessory"
 }, {
   "id": 5,
   "name": "Kids Unisex Helmet",
   "price": 3.50,
-  "image": "https://doc-00-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/ae1a03s5litld35dla73grtqcsgea6bm/1569780000000/02903672740124267563/02903672740124267563/16DAt2ZOXN3Rjq2HlaoX-d-GJ5xT6Avq4?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=16DAt2ZOXN3Rjq2HlaoX-d-GJ5xT6Avq4",
   "product_type": "accessory"
 }, {
   "id": 6,
   "name": "Insurance",
   "price": 9.99,
-  "image": "https://doc-08-44-docs.googleusercontent.com/docs/securesc/4ulrouemhgojv70t3u6ramukj8d10iba/j6tcq7huipvkp0affgkr80t664pdg7cj/1569780000000/02903672740124267563/02903672740124267563/1Jnw1d59DgofpaW8JIAzhyaZ96tv_rXqB?e=view",
+  "image": "http://drive.google.com/uc?export=view&id=1Jnw1d59DgofpaW8JIAzhyaZ96tv_rXqB",
   "product_type": "addon"
 }];
 module.exports = {
@@ -47983,7 +47983,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64624" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61320" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
